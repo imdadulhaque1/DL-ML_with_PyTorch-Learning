79f07f78992fb4cc63e1a6a2b9ef0a360a86d0fc
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-03-03 11:55:49 +0600

    Initialize and convert Tensor to other Types(init, float, double)

--- PyTorchPracticing.py
+++ PyTorchPracticing.py
@@ -1,8 +1,8 @@
 import torch
-#=============================================================================#
-#                                           Initializing Tensor                                                #
-#=============================================================================#
 
+#=========================================================#
+#                                     Initializing Tensor                                                #
+#=========================================================#
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 my_tensor = torch.tensor([[1, 2, 3], [5, 6, 7]], dtype=torch.float32,
@@ -15,9 +15,9 @@ my_tensor = torch.tensor([[1, 2, 3], [5, 6, 7]], dtype=torch.float32,
 # print(my_tensor.requires_grad)
 
 
-#=============================================================================#
+#=========================================================#
 #                        Other common initialization methods                               #
-#=============================================================================#
+#=========================================================#
 x = torch.empty(size=(3,3))
 x = torch.zeros((3,3))
 x = torch.rand((3,3))
@@ -28,4 +28,19 @@ x = torch.linspace(start=0.1, end=1, steps=10)
 x = torch.empty(size=(1,5)).normal_(mean=0, std=1)
 x = torch.empty(size=(1,5)).uniform_(0,1)
 x = torch.diag(torch.ones(3))
-print(x)
+# print(x)
+
+#=========================================================#
+#    How to initialize & convert tensors to other types(init, float, double)      #
+#=========================================================#
+tensor = torch.arange(4)
+print(tensor.bool())     # boolean True/False
+print(tensor.short())   # init16
+print(tensor.long())   # init64 (Important)
+print(tensor.half())   # float16
+print(tensor.float())   # float32 (Important)
+print(tensor.double())   # float64
+
+
+
+
